eebac0b632e51e429a43cdf8d615fdaab04d7f77
nixon <nixon128790@gmail.com>
2025-06-29 12:47:02 +0530

    Perbaikan Tampilan

--- netlify/functions/data.js
+++ netlify/functions/data.js
@@ -1,28 +1,32 @@
-// netlify/functions/data.js
+// netlify/functions/data-spt.js
+const PizZip = require("pizzip");
+const Docxtemplater = require("docxtemplater");
+const fs = require("fs");
+const path = require("path");
 const data = {
     pegawai: [
         { "nama": "Zalliawaty, S.Sos.", "nip": "19750824 200003 2 003", "pangkat": "Pembina Muda/ IV.c", "jabatan": "PPUPD MADYA" },
         { "nama": "M. Sjaifoedin Z.H.P, S.T.", "nip": "19650613 199703 1 002", "pangkat": "Pembina Tingkat I/ IV.b", "jabatan": "PPUPD MADYA" },
         { "nama": "Muh. Akbar Sholeh, S.Si., M.S.Ak.", "nip": "19800812 200312 1 006", "pangkat": "Pembina Muda/ IV.c", "jabatan": "AUDITOR MADYA" },
-        // ... dan seterusnya ...
+        // ... PASTIkan Anda menggunakan data lengkap Anda di sini ...
     ],
     pejabat: [
         { "nama": "Dra. Bayana, M.Si., CGCEA", "pangkat": "Pembina Utama Madya/ IV.d", "nip": "19690401 199003 2 004", "jabatan": "I N S P E K T U R" },
         { "nama": "Dra. Hidayatika, M.Si.", "pangkat": "Pembina Utama Muda/ IV.c", "nip": "19680716 198909 2 002", "jabatan": "Sekretaris" },
         { "nama": "Syamsurialsyah, S.Pt., M.T.", "pangkat": "Pembina Tingkat I/ IV.b", "nip": "19721112 200003 1 006", "jabatan": "Inspektur Pembantu Wilayah I" },
-        // ... dan seterusnya ...
+        // ... PASTIkan Anda menggunakan data lengkap Anda di sini ...
     ],
     opd: [
         { nama: "Sekretariat DPRD", value: "Sekretariat DPRD" },
         { nama: "Biro Administrasi Pembangunan", value: "Biro Administrasi Pembangunan" },
         { nama: "Biro Hukum", value: "Biro Hukum" },
-        // ... dan seterusnya ...
+        // ... PASTIkan Anda menggunakan data lengkap Anda di sini ...
     ],
     alatAngkut: [
         { nama: "Mobil Dinas", value: "Mobil Dinas" },
         { nama: "Motor Dinas", value: "Motor Dinas" },
         { nama: "Kendaraan Pribadi", value: "Kendaraan Pribadi" },
-        // ... dan seterusnya ...
+        // ... PASTIkan Anda menggunakan data lengkap Anda di sini ...
     ],
     jenis_pengawasan: [
         {
@@ -40,7 +44,7 @@ const data = {
             value: "Reviu Rencana Kerja Perangkat Daerah Tahun 2026 (Renja PD)",
             dasar_hukum: "Peraturan Menteri Dalam Negeri Nomor 86 Tahun 2017..."
         },
-        // ... dan seterusnya ...
+        // ... PASTIkan Anda menggunakan data lengkap Anda di sini ...
     ]
 };
 
